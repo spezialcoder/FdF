c16156fe0a5dd51d14666b9f20017c1842d95dc6
lsorg <lsorg@student.42heilbronn.de>
2024-06-01 20:43:43 +0200

    clean it

--- fdf_engine.c
+++
@@ -1,6 +0,0 @@
-//
-// Created by Lewin Sorg on 4/22/24.
-//
-
-#include "fdf_engine.h"
-
--- fdf_engine.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Lewin Sorg on 4/22/24.
-//
-
-#ifndef FDF_FDF_ENGINE_H
-#define FDF_FDF_ENGINE_H
-
-#endif //FDF_FDF_ENGINE_H
